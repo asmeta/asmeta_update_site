e988d561652ee0fba46acbc1e2c121aa4b38bd8e
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-05-17 08:51:29 +0200

    first update site

---
+++ test_update_site.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# save the current location
+updatesite=$(pwd)
+
+# make a remp dir
+#mkdir temp
+cd temp
+# first download eclipse
+# use he mirror service, skip if it alredy exits 
+#wget --no-clobber http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/2020-03/R/eclipse-java-2020-03-R-win32-x86_64.zip
+
+#unzip it
+#unzip eclipse-java-2020-03-R-win32-x86_64.zip
+
+# install the plugins
+cd eclipse
+ls ../../../asmeta_update_site
+./eclipsec.exe -noSplash -application org.eclipse.equinox.p2.director  -repository file://$updatesite  -installIU Asmeta
+# delete temp
+#rm -rf temp
