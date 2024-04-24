9868f3e74d9aa3ddbad897adcc0332e12a389215
Angelo Gargantini <angelo.gargantini@unibg.it>
2024-04-24 10:54:54 +0200

    version 24.04

--- test_update_site.sh
+++ test_update_site.sh
@@ -4,9 +4,15 @@
 updatesite=$(pwd)
 echo "$updatesite" 
 
+<<<<<<< Updated upstream
 eclipseurldir=http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/2023-12/R/
 
 eclipsezip=eclipse-java-2023-12-R-win32-x86_64.zip
+=======
+eclipseurldir=http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/2024-03/R/
+
+eclipsezip=eclipse-java-2024-03-R-win32-x86_64.zip
+>>>>>>> Stashed changes
 
 # make a temp dir
 #mkdir temp
@@ -34,11 +40,11 @@ cd eclipse_fresh
 # ./eclipsec.exe -noSplash -application org.eclipse.equinox.p2.director  -repository file:/home/garganti/progettiDaSVN/asmeta/asmeta_update_site  -installIU Asmeta
 # uso i path di windows
 # questo sul fisso dell'ufficio
-# baseasmetalocale=D:\\AgHome\\progettidaSVNGIT
+baseasmetalocale=D:\\AgHome\\progettidaSVNGIT
 # questo da casa
 # baseasmetalocale=D:\\AgDocuments\\progettiDaSVN
 # questo dal portatile
-baseasmetalocale=C:\\Users\\angel\\codicefromrepos\\ricerca
+# baseasmetalocale=C:\\Users\\angel\\codicefromrepos\\ricerca
 # 
 # adesso prova ad installare
 #
